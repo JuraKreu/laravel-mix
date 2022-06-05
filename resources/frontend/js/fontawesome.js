ecdc3e89251a68d708a3c911346aaa8091b04a15
jurak <Hivq9zdS79m2!>
2022-06-05 15:55:57 +0200

    :lipstick: adding new documentation

--- resources/frontend/js/fontawesome.js
+++ resources/frontend/js/fontawesome.js
@@ -1,8 +1,7 @@
 // Load Fontawesome Icons //
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faBootstrap, faSass, faLaravel } from '@fortawesome/free-brands-svg-icons'
-import { faCube } from '@fortawesome/pro-thin-svg-icons'
-import { faFontAwesome } from '@fortawesome/pro-thin-svg-icons'
+import { faCube, faFontAwesome } from '@fortawesome/pro-thin-svg-icons'
 
 library.add(faBootstrap, faSass, faCube, faLaravel, faFontAwesome)
 
